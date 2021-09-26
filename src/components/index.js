23a06942411fb1b38f1954b73e80ce18cf484a33
ChyngyzOrmoshev <chyngyzormoshev@gmail.com>
2021-09-26 10:47:54 +0600

    каркас приложения и верстка фона

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+export { default as Header } from "../components/header/Header";
+export { default as TopBar } from "../components/topbar/TopBar";
+
+export { default as Logo } from "../assets/img/Logo.png";
+export { default as info } from "../assets/img/information.svg";
+export { default as services } from "../assets/img/services.svg";
+export { default as portfolio } from "../assets/img/portfolio.svg";
+export { default as telegram } from "../assets/img/icon_telegram.svg";
+export { default as gmail } from "../assets/img/icon_gmail.svg";
+export { default as instagram } from "../assets/img/icon_instagram.svg";
+export { default as facebook } from "../assets/img/icon_facebook.svg";
